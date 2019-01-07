ecb75a024eef1985887e62215aaa38298c0d51ba
Dave Gurnell <dave@untyped.com>
2019-01-07 12:02:47 +0000

    Fix name of dist directory

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 if [ -z "$(git status --porcelain)" ]; then
   # Working directory clean
-  git subtree split --prefix docs -b gh-pages
+  git subtree split --prefix dist -b gh-pages
   git push -f origin gh-pages:gh-pages
   git branch -D gh-pages
 else
   # Uncommitted changes
   echo 'ERROR! Uncommitted changes in the working copy.'
-  echo 'Please commit all outstanding changes and run again to deploy from `/docs`.'
+  echo 'Please commit all outstanding changes and run again to deploy from `/dist`.'
 fi
